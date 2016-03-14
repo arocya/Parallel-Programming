31012806db5dee4d4325717770442538b2180d36
arocya <arion.almond@gmail.com>
2016-03-14 16:11:26 -0400

    Initial Commit Update

---
+++ firstProject.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : firstProject.c
+ Author      : Arion Almond
+ Version     : 1.0
+ Copyright   : Open Source
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
